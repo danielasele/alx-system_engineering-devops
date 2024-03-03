20210cd2fac493bd238b2d44b3b7b27ff9e9e3f8
danielasele <daniase2113@gmail.com>
2024-03-03 13:38:17 -0800

    0-simply_match_school.rb task updated

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -3,7 +3,7 @@
 # Check if the argument contains the word "School"
 if ARGV[0].match?(/\bSchool\b/)
   # If it contains the word "School", print the matched string
-  puts ARGV[0]
+  puts ARGV[0].scan(/\bSchool\b/).join
 else
   # If it doesn't contain the word "School", print nothing
   puts ""
