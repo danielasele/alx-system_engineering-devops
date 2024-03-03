ab5356a3c0a2e4680f42fd28976fdeb430f6612c
danielasele <daniase2113@gmail.com>
2024-03-03 14:25:28 -0800

    2-repetition_token_1.rb updated

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,10 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if the argument matches the regular expression pattern
-if ARGV[0].match?(/^hbt+n$/)
-  # If it matches, print the matched string
-  puts ARGV[0]
-else
-  # If it doesn't match, print nothing
-  puts ""
-end
+puts ARGV[0].scan(/hb?t?n/).join
