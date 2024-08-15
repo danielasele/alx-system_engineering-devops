cd469caa629f6fc76dff0be0ec95d9e8632c5cf0
danielasele <daniase2113@gmail.com>
2024-08-15 00:55:30 -0700

    0-subs.py task

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script that queries the Reddit API to get the number of subscribers
+Script to query the Reddit API and retrieve the number of subscribers
 for a given subreddit.
 """
 
@@ -8,15 +8,19 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0 (compatible; Subreddit-Subscriber-Count/1.0)"}
+    """Return the total number of subscribers for a given subreddit."""
+    if not subreddit or type(subreddit) is not str:
+        return (0)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (compatible; Subreddit-Subscriber-Count/1.0)"
+    }
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
-            data = response.json()
-            return (data['data'].get('subscribers', 0))
-        else:
-            return (0)
+            data = response.json().get('data', {})
+            return (data.get('subscribers', 0))
+        return (0)
     except requests.RequestException:
         return (0)
